ef536886ed0c71d53e53d5ea3781d521bad98c7a
Luan Reinhold <anjobranco67@gmail.com>
2022-06-01 11:42:26 -0300

    add verificacao no filme

--- API/src/controller/FilmeController.js
+++ API/src/controller/FilmeController.js
@@ -7,12 +7,30 @@ server.post('/filme', async (req, resp) => {
     try {
         const novoFilme = req.body;
 
+        if(!novoFilme.nome)
+        throw new Error ("Campo de nome Obrigatório!")
+
+        if(!novoFilme.sinopse)
+        throw new Error ("Campo de sinopse Obrigatório!")
+
+        if(!novoFilme.avaliacao)
+        throw new Error ("Campo de avalição Obrigatório!")
+
+        if(!novoFilme.lancamento)
+        throw new Error ("Campo de lançamento Obrigatório!")
+
+        if(!novoFilme.disponivel)
+        throw new Error ("Campo disponivel Obrigatório!")
+        
+        if(!novoFilme.usuario)
+        throw new Error ("Campo de usuario Obrigatório!")
+
         const filmeInserido = await InserirFilme(novoFilme);
 
         resp.send(filmeInserido)
 
     } catch (err) {
-        resp.send(400).send({
+        resp.status(400).send({
             erro: err.message
         })
     }
